6d31eccd12af266f95af9d51ae4076b0fd9347dd
BottomHalf <istiyaq.mi9@gmail.com>
2024-08-21 20:13:51 +0530

    new

---
+++ src/main/java/in/bottomhalf/utility/data/controller/CheckRequestController.java
@@ -0,0 +1,17 @@
+package in.bottomhalf.utility.data.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/check/")
+public class CheckRequestController {
+
+    @GetMapping(value = "get")
+    public ResponseEntity<String> getTestData() {
+        String message = "Endpoint is working.";
+        return ResponseEntity.ok(message);
+    }
+}
---
+++ src/main/java/in/bottomhalf/utility/data/service/DbManager.java
@@ -0,0 +1,50 @@
+package in.bottomhalf.utility.data.service;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import in.bottomhalf.utility.data.utils.Template;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class DbManager {
+
+    @Autowired
+    DbUtils dbUtils;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    DbManager(Template template) {
+        jdbcTemplate = template.getTemplate();
+    }
+
+    public <T> void save(T instance) throws Exception {
+        String query = dbUtils.save(instance);
+        jdbcTemplate.execute(query);
+    }
+
+    public <T> List<T> get(Class<T> type) throws Exception {
+        String query = dbUtils.get(type);
+        List<Map<String, Object>> result = jdbcTemplate.queryForList(query);
+
+        return mapper.convertValue(result, new TypeReference<List<T>>() {});
+    }
+
+    public <T> T getById(T instance, Class<T> type) throws Exception {
+        String query = dbUtils.getById(type);
+
+        T emp = jdbcTemplate.queryForObject(query, type);
+
+        List<Map<String, Object>> result = jdbcTemplate.queryForList(query);
+
+        return mapper.convertValue(result, new TypeReference<T>() {});
+    }
+}
