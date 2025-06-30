8a4a1b10930d4e800956526711ba88d96b6d7cf9
BottomHalf <istiyaq.mi9@gmail.com>
2025-06-30 18:38:43 +0530

    pom modified

--- src/main/java/in/bottomhalf/data/Application.java
+++ src/main/java/in/bottomhalf/data/Application.java
@@ -3,10 +3,12 @@ package in.bottomhalf.data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import in.bottomhalf;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Application {
 
+	RequestMicroservice
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
